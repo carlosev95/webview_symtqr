e6c2a501b1e3829e1e527a1bc0b67611f239649d
Carlos E <cromo_lp@hotmail.com>
2023-08-11 16:55:55 -0600

    Set permissions and constants for run webview

--- app/src/main/java/com/example/symtqr/MainActivity.kt
+++ app/src/main/java/com/example/symtqr/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.symtqr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
 import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val visor = findViewById<WebView>(R.id.web)
+
+        visor.webChromeClient = object : WebChromeClient(){
+
+        }
+
+        visor.webViewClient = object : WebViewClient(){
+
+        }
+
+        val settings:WebSettings = visor.settings
+        settings.javaScriptEnabled = true
+
+        visor.loadUrl("https://www.google.com")
     }
 }
